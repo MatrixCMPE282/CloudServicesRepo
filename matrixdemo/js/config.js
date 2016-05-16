9dd14ba9948e55bc998018690b5b66673f56643d
nehap12 <neha.paul@sjsu.edu>
2016-05-15 20:28:11 -0700

    Commit new directory

---
+++ matrixdemo/js/config.js
@@ -0,0 +1,8 @@
+window.config = {
+  role: 'arn:aws:iam::739226609075:role/access-to-s3',
+  principal: 'arn:aws:iam::739226609075:saml-provider/SAMLAUTH0',
+  domain: 'matrix4.auth0.com',
+  clientID: 'hRRoR9eAvJ8towB7IHvntV9zhAVCElmh',
+  targetClientId: 'hRRoR9eAvJ8towB7IHvntV9zhAVCElmh',
+  region: 'us-west-2'
+};
